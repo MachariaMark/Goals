31fe1bdb87736afe2459dd666aede3f94726f7ba
MachariaMark <official1offmark@gmail.com>
2019-09-19 18:24:10 +0300

    error fixes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -14,22 +14,34 @@ export class GoalComponent implements OnInit {
    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019, 9,9)),
    new Goal(5,'Solve math homework','Damn Math', new Date(2019, 10, 10)),
    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,11,11)),];
-    toggleDetails(index){
-       this.goals[index].showDescription = !this.goals[index].showDescription;
-    }
- completeGoal(isComplete, index){
-   if(isComplete){
-     this.goals.splice(index,1);
+  
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(isComplete, index){
+    if(isComplete){
+      this.goals.splice(index,1);
    }
- }
+  }
 
- addNewGoal(goal){
-   let goalLength = this.goals.length;
-   goal.id = goalLength+1;
-   goal.completeDate = new Date(goal.completeDate)
-   this.goals.push(goal)
- }
- constructor() { }
- ngOnInit() {
- }
+  deleteGoal(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
+    }
+  }
+
+  addNewGoal(goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
